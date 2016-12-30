6938a91c18a66ca48022e71bcb41e326de7706f1
Alexander Efimov <teeeses@mail.ru>
2016-12-30 15:09:43 +0300

    \\

--- CatchGame/app/src/main/java/com/explead/catchcage/UtilsGame.java
+++ CatchGame/app/src/main/java/com/explead/catchcage/UtilsGame.java
@@ -41,12 +41,14 @@ public class UtilsGame {
     public int checkWon(int[][] field) {
         for(int i = 5; i >= 0; i++) {
             for(int y = 0; y < 7; y++) {
-                int count1 = checkPoint(i, y, field[i][y], "up");
-                int count2 = checkPoint(i, y, field[i][y], "diagonal");
-                int count3 = checkPoint(i, y, field[i][y], "right");
-                if(count1 >= 4 || count2 >= 4 || count3 >= 4) {
-                    System.out.println("Won: " + field[i][y]);
-                    return field[i][y];
+                if(field[i][y] != 0) {
+                    int count1 = checkPoint(i, y, field[i][y], field, "up");
+                    int count2 = checkPoint(i, y, field[i][y], field, "diagonal");
+                    int count3 = checkPoint(i, y, field[i][y], field, "right");
+                    if (count1 >= 4 || count2 >= 4 || count3 >= 4) {
+                        System.out.println("Won: " + field[i][y]);
+                        return field[i][y];
+                    }
                 }
             }
         }
@@ -54,21 +56,21 @@ public class UtilsGame {
         return 0;
     }
 
-    public int checkPoint(int x, int y, int value, String str) {
+    public int checkPoint(int x, int y, int value, int[][] field, String str) {
         int count = 1;
         if(x-1 >= 0 && str.equals("up")) {
             if(value == field[x-1][y]) {
-                checkPoint(x - 1, y, field[x - 1][y], "up");
+                checkPoint(x - 1, y, field[x - 1][y], field, "up");
                 count++;
             }
-        } else if(x-1 >= 0 && y+1 <= 6 && str.equals("diagonal")) {
+        } else if(x-1 >= 0 && y+1 < 6 && str.equals("diagonal")) {
             if(value == field[x - 1][y + 1]) {
-                checkPoint(x - 1, y + 1, field[x - 1][y + 1], "diagonal");
+                checkPoint(x - 1, y + 1, field[x - 1][y + 1], field, "diagonal");
                 count++;
             }
-        } else if(y+1 <= 6 && str.equals("right")) {
+        } else if(y+1 < 6 && str.equals("right")) {
             if(value == field[x][y + 1]) {
-                checkPoint(x, y + 1, field[x][y + 1], "right");
+                checkPoint(x, y + 1, field[x][y + 1], field, "right");
                 count++;
             }
         }
